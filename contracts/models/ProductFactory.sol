40805c1fb238aabd9f7713d7d1cfb25ddce16c07
Mohamed EL BARCANI <melbarcani@github.com>
2021-11-16 11:37:24 +0100

    owner

--- contracts/models/ProductFactory.sol
+++ contracts/models/ProductFactory.sol
@@ -11,14 +11,19 @@ contract ProductFactory {
         string matricule;
         string gender;
         string comment;
+        uint farmerId;
     }
 
     Animal[] public animals;
 
-    function createAnimal(string memory origin, uint256 birthDate, string memory matricule, string memory gender, string memory comment) public {
+    function createAnimal(string memory origin, uint256 memory birthDate, string memory matricule, string memory gender, string memory comment) public {
         uint id = animals.push(Animal(origin, birthDate, matricule, gender, comment));
         emit NewAnimal(id, matricule);
     }
 
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
 }
